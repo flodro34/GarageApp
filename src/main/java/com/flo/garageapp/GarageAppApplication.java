153e2b2285a7b967648f560957ff5476dccdeb68
flodro34 <f.derosso@gmail.com>
2024-05-01 19:34:09 +0200

    add connexion bdd

---
+++ src/main/java/com/flo/garageapp/GarageAppApplication.java
@@ -0,0 +1,14 @@
+package com.flo.garageapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageAppApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GarageAppApplication.class, args);
+    }
+
+}
